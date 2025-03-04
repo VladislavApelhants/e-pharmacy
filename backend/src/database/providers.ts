4cdb200d174dcbc78c8810c2bd7c0b17b09eda67
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2025-03-05 00:14:11 +0200

    02

--- backend/src/database/providers.ts
+++ backend/src/database/providers.ts
@@ -1,10 +1,14 @@
 import * as mongoose from 'mongoose';
 import { validateEnvVariable } from 'src/utils/env.util';
+import "dotenv/config"
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect(validateEnvVariable(process.env.DB_URI, 'DB_URI')),
+    useFactory: async (): Promise<typeof mongoose> => {
+      console.log("process.env.DB_URI",process.env.DB_URI)
+
+      return await mongoose.connect(validateEnvVariable(process.env.DB_URI, 'DB_URI'))
+    },
   },
 ];
